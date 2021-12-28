806ac088632251d2b39e01f58169ec5b695dfc8a
devmil <info@devmil.de>
2021-12-29 00:02:25 +0100

    xterm lib: exports theme related types

--- lib/xterm.dart
+++ lib/xterm.dart
@@ -1,7 +1,9 @@
 library xterm;
 
+export 'terminal/platform.dart';
 export 'terminal/terminal.dart';
 export 'terminal/terminal_backend.dart';
 export 'terminal/terminal_ui_interaction.dart';
-export 'terminal/platform.dart';
 export 'theme/terminal_style.dart';
+export 'theme/terminal_theme.dart';
+export 'theme/terminal_themes.dart';
